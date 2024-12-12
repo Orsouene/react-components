259e755f40b239f60c5b5ad1e86cbd7f34fa1b91
Orsouene <orsoueneelaouizeb@gmail.com>
2024-12-12 15:46:22 +0100

    Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+// importato Header
 import Header from "./Components/Header";
+// importato Main
 import Main from "./Components/Main";
-
+// importato Footer
+import Footer from "./Components/Footer";
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
