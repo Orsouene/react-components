4d14d95a93d67fca63c154bb81c1ef917e7a2234
Orsouene <orsoueneelaouizeb@gmail.com>
2024-12-12 15:24:45 +0100

    Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-// importo la funzione header
 import Header from "./Components/Header";
+import Main from "./Components/Main";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ src/Components/Main.jsx
@@ -0,0 +1,18 @@
+function Main() {
+  return (
+    <main>
+      <div className="card">
+        <img src="/img/grey.jpg" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Titolo del posto</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <button href="#">LEGGI DI PIU</button>
+        </div>
+      </div>
+    </main>
+  );
+}
+export default Main;
